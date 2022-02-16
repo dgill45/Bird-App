c96728172f0dc5e4da3718230251ca1e153a20d2
donellgill <donell.gill@outlook.com>
2022-02-16 18:46:27 -0500

    added update action, likes to bird table and increment method to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create] 
+  resources :birds, only: [:index, :show, :create, :update] 
+  patch "/birds/:id/like", to: "birds#increment_likes"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
